ed23416788daf28def50bdc7140f4bb0e0c8cf38
Mahbub-limon <mahbublimon1127@gmail.com>
2024-10-31 00:54:06 +0600

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/ModelsClasses/")]
+    public class HomeController : Controller
+    {
+    
+        [HttpPost]
+        public IActionResult CreateUser([FromBody] ModelClass newHomeController)
+        {
+
+            return Ok($"User {newHomeController.Name} created , Age : {newHomeController.Age} ");
+        }
+    }
+}
